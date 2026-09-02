114c9cbb508caf148298d7eb39c79577b85e6a48
Aditya Chauhan <adityachauhan9226.work@gmail.com>
2026-09-02 22:47:53 +0530

    Time: 0 ms (100%), Space: 22.8 MB (78.2%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+        ListNode* prev = nullptr;
+        while (second) {
+            ListNode* next = second->next;
+            second->next = prev;
+            prev = second;
+            second = next;
+        }
+        second = prev;
+        ListNode* first = head;
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+            first->next = second;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
